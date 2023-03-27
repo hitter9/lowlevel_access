ad712593dc4d82f1d1b5a78d694906f75e3c4d63
firsovvictor <firsov25112000@gmail.com>
2023-03-27 11:01:25 +0500

    Тестовая версия программы чтения диска

--- lowlevel_access.cpp
+++ lowlevel_access.cpp
@@ -1,8 +1,32 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    setlocale(LC_ALL, "Ru");
+    auto filename = L"\\\\.\\C:";
+    auto opendisk = CreateFileW(filename, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (opendisk == INVALID_HANDLE_VALUE)
+    {
+        cout << "Ошибка открытия диска" << endl;
+        return 1;
+    }
+    BYTE* dataBuffer = new BYTE[512];
+    DWORD readBytes = 512;
+    DWORD readedBytes;
+    auto readResult = ReadFile(opendisk, dataBuffer, readBytes, &readedBytes, NULL);
+    if (!readResult || readedBytes != readBytes)
+    {
+        cout << "Ошибка чтения диска" << endl;
+        return 2;
+    }
+    for (int i = 0; i < readedBytes; i++)
+        cout << dataBuffer[i];
+    cout << endl;
+    delete[] dataBuffer;
+    CloseHandle(opendisk);
+    system("pause");
+    return 0;
 }
